7be372396972fd302ab487b036a207229524000b
Ketan Patil <100.percent.ketan@gmail.com>
2020-08-08 18:54:06 +0530

    enabling editor.js and relevant pluggins

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,3 +7,68 @@ console.log('\'Allo \'Allo!');
 // Uncomment to enable Bootstrap popovers
 // https://getbootstrap.com/docs/4.0/components/popovers/#example-enable-popovers-everywhere
 // $(function () { $('[data-toggle="popover"]').popover(); });
+
+
+const editor = new EditorJS({
+  holder: 'editor',
+  tools: {
+    header: {
+      class: Header,
+      config: {
+        placeholder: 'Enter a header',
+        levels: [1, 2, 3, 4, 5, 6],
+        defaultLevel: 3
+      }
+    },
+    code: CodeTool,
+    delimiter: Delimiter,
+    checklist: {
+      class: Checklist,
+      inlineToolbar: true,
+    },
+    list: {
+      class: List,
+      inlineToolbar: true,
+    },
+    quote: {
+      class: Quote,
+      inlineToolbar: true,
+      shortcut: 'CMD+SHIFT+O',
+      config: {
+        quotePlaceholder: 'Enter a quote',
+        captionPlaceholder: 'Quote\'s author',
+      },
+    },
+    image: SimpleImage,
+    table: {
+      class: Table,
+      inlineToolbar: true,
+      config: {
+        rows: 5,
+        cols: 4,
+      },
+    },
+    inlineCode: {
+      class: InlineCode,
+      shortcut: 'CMD+SHIFT+M',
+    },
+    paragraph: {
+      class: Paragraph,
+      inlineToolbar: true,
+    },
+    warning: {
+      class: Warning,
+      inlineToolbar: true,
+      shortcut: 'CMD+SHIFT+W',
+      config: {
+        titlePlaceholder: 'Title',
+        messagePlaceholder: 'Message',
+      },
+    },
+    Marker: {
+      class: Marker,
+      shortcut: 'CMD+SHIFT+M',
+    },
+    underline: Underline
+  }
+});
